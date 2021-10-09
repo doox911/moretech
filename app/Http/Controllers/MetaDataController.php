2f46b5a232ea75486ca4b1a2d233389c27bdb2a7
rodion <almetov.r@stis.ru>
2021-10-09 21:20:59 +0300

    sql

--- app/Http/Controllers/MetaDataController.php
+++ app/Http/Controllers/MetaDataController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Class\MetaDataGetter;
+use App\Classes\MetaDataGetter;
 use App\Classes\DataSetsGetter;
 use App\Http\Requests\DataSetQueryRequest;
+use CommandService;
+use Illuminate\Http\JsonResponse;
 
 class MetaDataController {
 
@@ -13,7 +15,7 @@ class MetaDataController {
    * @return \Illuminate\Http\JsonResponse
    * @deprecated пока не работает
    */
-  public function getCatalogs(): \Illuminate\Http\JsonResponse {
+  public function getCatalogs(): JsonResponse {
     $dsg = new DataSetsGetter;
 
 
@@ -37,22 +39,68 @@ class MetaDataController {
 
     $queries = [];
     foreach ($select_fields as $select_field) {
+      $field = $select_field['dataset_name'] . '.' . $select_field['field_name'];
       if (!isset($queries[$select_field['dataset_name']])) {
-        $queries[$select_field['dataset_name']] = "select {$select_field['field_name']}";
+        $queries[$select_field['dataset_name']] = "select $field";
       } else {
-        $queries[$select_field['dataset_name']] .= ",{$select_field['field_name']}";
+        $queries[$select_field['dataset_name']] .= ",$field";
       }
     }
 
-    dd($queries);
 
     // todo where
-    $where = $data['where'];
+    $where_fields = $data['where'];
+    foreach ($where_fields as $where_field) {
+      $query = &$queries[$where_field['field']['dataset_name']];
+      if (!isset($query) || ($query && !strstr($query, 'where'))) {
+
+        if (!strstr($query, 'select')) {
+          $query = "select *";
+        }
+
+        $query .= " from {$where_field['field']['dataset_name']} where ";
+      } else {
+        $query .= " and ";
+      }
+      $field = $where_field['field']['dataset_name'] . '.' . $where_field['field']['field_name'];
+
+      $query .= "$field {$where_field['condition']} {$where_field['value']}";
+    }
+
+    unset($query);
 
     // todo sort
-    $sort = $data['sort'];
+    $sort_fields = $data['sort'];
+
+    foreach ($sort_fields as $sort_field) {
+      $query = &$queries[$sort_field['dataset_name']];
+
+      if (!$query || !strstr($query, 'select')) {
+        $query = "select *";
+      }
+
+      if (!strstr($query, 'from')) {
+        $query .= " from {$sort_field['dataset_name']}";
+      }
+
+      $field = $sort_field['dataset_name'] . '.' . $sort_field['field_name'];
+
+      if (!strstr($query, 'order by')) {
+        $query .= " ORDER BY $field";
+      } else {
+        $query .= ", $field";
+      }
+    }
 
 
-    return $query;
+    $cs = (new CommandService($queries))->run();
+
+
+    return response()->json([
+      'message' => 'Все запросы успешно отправлены в очередь',
+      'content' => [
+        'queries' => $cs
+      ]
+    ]);
   }
 }
